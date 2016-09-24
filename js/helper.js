11a4578e39cc72f3857f1c8f33ec46e852f6c3e0
Chris Seraph <Chris Seraph>
2016-09-24 17:26:27 +0400

    re-adjusted line. added projects.projects. fixed loop.

--- js/helper.js
+++ js/helper.js
@@ -48,8 +48,8 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<h3 href="#">%data%';
-var HTMLschoolDegree = ' - %data%</a>';
+var HTMLschoolName = '<h3><a href="#" target="_blank">%data%';
+var HTMLschoolDegree = ' - %data%</a></h3>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,10 +8,7 @@ var bio = {
     "twitter": "angelocandari1",
     "location": "Busan"
   },
-  "welcomeMessage": "Specializing in Digital Marketing with a background of Web \
-  Development and Data Analytics. I am a Google Analytics and Google Search \
-  Adwords Certified professional with a Bachelor's Degree in Communications \
-  Technology Management.",
+  "welcomeMessage": "Specializing in Digital Marketing with a background of Web Development and Data Analytics. I am a Google Analytics and Google Search Adwords Certified professional with a Bachelor's Degree in Communications Technology Management.",
   "skills": ["Digital Marketing", "Web Development", "Data Analytics"],
   "biopic": "images/pic.jpg"
 };
@@ -51,102 +48,52 @@ var work = {
     "title": "Ecommerce Executive",
     "location": "Dubai, United Arab Emirates",
     "dates": "May 2014 to September 2016",
-    "description": "Developed critical digital marketing strategies and \
-    implemented tactics including display advertising, native ads, \
-    SEM (SEO&PPC), email marketing, link -building, mobile and social \
-    media marketing generating brand awareness and increasing user \
-    engagement as well as driving online sales. \
-    Prepares concrete and actionable Ecommerce Reports that determines \
-    performance of online campaigns and website landing pages integrity. \
-    Manages Digital Brand of The Address Dubai Mall based on the overarching \
-    brand voice of The Address Resorts + Hotels. Improves conversions of \
-    campaigns and share of voice of our brand across the digital space. \
-    Liaise with third-party agencies/vendors and manages budgets for a \
-    cost-effective digital campaign. Maintains relationships with online \
-    content providers, creative agencies, online PR and PPC experts. \
-    Transformed a relatively non-existent social media profile into an online \
-    presence across Twitter, Facebook, Google+ and Instagram, TripAdvisor and \
-    other Online Travel Agents. Monitors performance for digital campaigns \
-    with Google Analytics, Sprinklr, Social Bakers, Radian6, Revinate, \
-    Hootsuite and Tweetdeck."
+    "description": "Developed critical digital marketing strategies and implemented tactics including display advertising, native ads, SEM (SEO&PPC), email marketing, link -building, mobile and social media marketing generating brand awareness and increasing user engagement as well as driving online sales. Prepares concrete and actionable Ecommerce Reports that determines performance of online campaigns and website landing pages integrity. Manages Digital Brand of The Address Dubai Mall based on the overarching brand voice of The Address Resorts + Hotels. Improves conversions of campaigns and share of voice of our brand across the digital space. Liaise with third-party agencies/vendors and manages budgets for a  cost-effective digital campaign. Maintains relationships with online content providers, creative agencies, online PR and PPC experts. Transformed a relatively non-existent social media profile into an online presence across Twitter, Facebook, Google+ and Instagram, TripAdvisor and other Online Travel Agents. Monitors performance for digital campaigns with Google Analytics, Sprinklr, Social Bakers, Radian6, Revinate, Hootsuite and Tweetdeck."
   }, {
     "employer": "Etisalat - Emirates Data Clearing House",
     "title": "Product Manager",
     "location": "Dubai, United Arab Emirates",
     "dates": "December 2011 to May 2014",
-    "description": " Managed the development of Revenue Assurance Roaming \
-    services. Organized creative campaign for new products and services. \
-    Conducts training for sales team on new services. Translates technical \
-    documents and writes commercial documents for new services. \
-    Conceptualize new services and initiate negotiations with partners on new \
-    services. Gathers technical data and industry compliance required for \
-    commercial implementation. Delegates tasks to IT team for development of \
-    services."
+    "description": " Managed the development of Revenue Assurance Roaming services. Organized creative campaign for new products and services. Conducts training for sales team on new services. Translates technical documents and writes commercial documents for new services. Conceptualize new services and initiate negotiations with partners on new services. Gathers technical data and industry compliance required for commercial implementation. Delegates tasks to IT team for development of services."
   }, {
     "employer": "Smart Communications Technology",
     "title": "Technical Trainer",
     "location": "Manila, Philippines",
     "dates": "January 2011 to December 2011",
-    "description": "Act as the main go-to person for all product trainings \
-    and managing all technical enquiries from customers or sales teams \
-    Organized and implemented product trainings with nationwide sales team \
-    Streamlined training process that has cut execution period  \
-    from 3 days to 1 day. Provided necessary documentation in support of sale \
-    teams. Improved web training (eLearning) compliance rate from 50% to 98% \
-    Supervised sales team in ensuring that presentation/marketing materials \
-    are updated and relevant to sales and marketing activities"
+    "description": "Act as the main go-to person for all product trainings and managing all technical enquiries from customers or sales teams Organized and implemented product trainings with nationwide sales team Streamlined training process that has cut execution period from 3 days to 1 day. Provided necessary documentation in support of sale teams. Improved web training (eLearning) compliance rate from 50% to 98% Supervised sales team in ensuring that presentation/marketing materials are updated and relevant to sales and marketing activities"
   }, {
     "employer": "Smart Communications Technology",
     "title": "Web Developer",
     "location": "Manila, Philippines",
     "dates": "October 2009 to January 2011",
-    "description": "Promoted to Technical Trainer. Designed and developed \
-    interactive training website. Produced training videos and online courses \
-    for new products and technical knowledge. Created product portfolios, \
-    sales presentations and marketing materials in support of sales team."
+    "description": "Promoted to Technical Trainer. Designed and developed interactive training website. Produced training videos and online courses for new products and technical knowledge. Created product portfolios, sales presentations and marketing materials in support of sales team."
   }, {
     "employer": "Rockwell Land Corporation",
     "title": "Marketing Associate",
     "location": "Manila, Philippines",
     "dates": "July 2008 to October 2009",
-    "description": "Supervised marketing roadshow events. Coordinated with \
-    advertising agencies on marketing execution. Negotiated with external \
-    partners on budget and prices. Created product portfolios, sales \
-    presentations and marketing materials in support of sales team."
+    "description": "Supervised marketing roadshow events. Coordinated with advertising agencies on marketing execution. Negotiated with external partners on budget and prices. Created product portfolios, sales presentations and marketing materials in support of sales team."
   }]
 };
 
-var projects = [{
-  "title": "Festive and NYE at The Address Dubai Mall 2015 Campaign",
-  "dates": "2015",
-  "description": "Developed Digital Marketing strategy for Christmas and New Year’s \
-    Eve Celebration at The Address Dubai Mall. Utilizing Social Media Ads, \
-    Display Ads, PPC Search Ads, Remarketing/Contextual Ads, Blogger \
-    Collaboration, Email we were able to generate high visitor traffic at our \
-    main website. With a combination of proper targeting, optimized bidding and \
-    clear call-to-action landing pages, we were able to generated leads and a \
-    healthy conversation rate that has generated 33% over our investment.",
-  "images": ["images/emaarFestive.jpg", "images/emaarNye.jpg"]
-}, {
-  "title": "Churrasco Campaign",
-  "dates": "2016",
-  "description": "Managed overall Marketing Campaign for Churrasco Night at Cabana. \
-    I prepared the digital marketing strategy for this campaign that spanned the whole \
-    year. And with my marketing manager resigning for the year of 2015, I was given the \
-    opportunity to plan and implement the whole year strategy to promote Cabana \
-    Churrasco Campaign from Creative Design Concept to Campaign implementation for \
-    Print, Radio and Online. The campaign continued throughout the year with a kick-off \
-    event and complimentary bursts of tactical campaigns that sustained awareness and \
-    convert potential customers.",
-  "images": ["images/emaarChurrasco.jpg", "images/emaarCabana.jpg"]
-}, {
-  "title": "Smart Bro Series",
-  "dates": "2011",
-  "description": "Collection of training materials to update Field Sales \
-    Agents on new products of Smart Bro. Includes training videos and \
-    interactive flash-based training materials.",
-  "images": ["images/smartSmartbro.jpg", "images/smartTechskills.jpg"]
-}];
+var projects = {
+  "projects": [{
+    "title": "Festive and NYE at The Address Dubai Mall 2015 Campaign",
+    "dates": "2015",
+    "description": "Developed Digital Marketing strategy for Christmas and New Year’s Eve Celebration at The Address Dubai Mall. Utilizing Social Media Ads, Display Ads, PPC Search Ads, Remarketing/Contextual Ads, Blogger Collaboration, Email we were able to generate high visitor traffic at our main website. With a combination of proper targeting, optimized bidding and clear call-to-action landing pages, we were able to generated leads and a healthy conversation rate that has generated 33% over our investment.",
+    "images": ["images/emaarFestive.jpg", "images/emaarNye.jpg"]
+  }, {
+    "title": "Churrasco Campaign",
+    "dates": "2016",
+    "description": "Managed overall Marketing Campaign for Churrasco Night at Cabana. I prepared the digital marketing strategy for this campaign that spanned the whole year. And with my marketing manager resigning for the year of 2015, I was given the opportunity to plan and implement the whole year strategy to promote Cabana Churrasco Campaign from Creative Design Concept to Campaign implementation for Print, Radio and Online. The campaign continued throughout the year with a kick-off event and complimentary bursts of tactical campaigns that sustained awareness and convert potential customers.",
+    "images": ["images/emaarChurrasco.jpg", "images/emaarCabana.jpg"]
+  }, {
+    "title": "Smart Bro Series",
+    "dates": "2011",
+    "description": "Collection of training materials to update Field Sales Agents on new products of Smart Bro. Includes training videos and interactive flash-based training materials.",
+    "images": ["images/smartSmartbro.jpg", "images/smartTechskills.jpg"]
+  }]
+};
 
 //Formats name to transform all letters from the Second Name to uppercase.
 function interNames(myName) {
@@ -180,7 +127,6 @@ bio.display = function() {
   addItems("#topContacts, #footerContacts", HTMLemail, bio.contacts.email);
   addItems("#topContacts, #footerContacts", HTMLgithub, bio.contacts.github);
   addItems("#topContacts, #footerContacts", HTMLtwitter, bio.contacts.twitter);
-  addLinks();
 
   addItems("#header", HTMLbioPic, bio.biopic);
   addItems("#header", HTMLwelcomeMsg, bio.welcomeMessage);
@@ -188,14 +134,13 @@ bio.display = function() {
   addRepeat("#skills", HTMLskills, bio.skills);
 
   // Adds hyperlinks to Contacts
-  function addLinks() {
-    $(".email").attr("href", ("mailto:" + bio.contacts.email));
-    $(".email").attr("target", "_blank");
-    $(".github").attr("href", ("https://github.com/" + bio.contacts.github));
-    $(".github").attr("target", "_blank");
-    $(".twitter").attr("href", ("https://twitter.com/" + bio.contacts.twitter));
-    $(".twitter").attr("target", "_blank");
-  }
+  $(".email").attr("href", ("mailto:" + bio.contacts.email));
+  $(".email").attr("target", "_blank");
+  $(".github").attr("href", ("https://github.com/" + bio.contacts.github));
+  $(".github").attr("target", "_blank");
+  $(".twitter").attr("href", ("https://twitter.com/" + bio.contacts.twitter));
+  $(".twitter").attr("target", "_blank");
+
 };
 
 bio.display();
@@ -222,8 +167,8 @@ work.display();
 projects.display = function() {
   $("#projects").append(HTMLprojectStart);
 
-  for (var i = 0; i < projects.length; i++) {
-    var key = projects[i];
+  for (var i = 0; i < projects.projects.length; i++) {
+    var key = projects.projects[i];
     addItems(".project-entry", HTMLprojectTitle, key.title);
     addItems(".project-entry", HTMLprojectDates, key.dates);
     addItems(".project-entry", HTMLprojectDescription, key.description);
@@ -247,39 +192,39 @@ education.display = function() {
   $("#education").append(HTMLschoolStart);
   for (var i = 0; i < education.schools.length; i++) {
     var keySchool = education.schools[i];
-    var formatSchoolName = HTMLschoolName.replace("%data%", keySchool.name);
+    var formatSchoolName = (HTMLschoolName.replace("#", keySchool.url)).replace("%data%", keySchool.name);
     var formatSchoolDegree = HTMLschoolDegree.replace("%data%", keySchool.degree);
     var formateTitleDegree = formatSchoolName + formatSchoolDegree;
-    $(".education-entry").append(formateTitleDegree);
-    addItems(".education-entry", HTMLschoolLocation, keySchool.location);
-    addItems(".education-entry", HTMLschoolDates, keySchool.dates);
-    addItems(".education-entry", HTMLschoolMajor, keySchool.majors);
+    $(".education-entry:last").append(formateTitleDegree);
+    addItems(".education-entry:last", HTMLschoolLocation, keySchool.location);
+    addItems(".education-entry:last", HTMLschoolDates, keySchool.dates);
+    addItems(".education-entry:last", HTMLschoolMajor, keySchool.majors);
   }
 
   //Online Classes
   $("#education").append(HTMLonlineClasses);
-  $("#education").append(HTMLonlineStart);
   for (var x = 0; x < education.onlineCourses.length; x++) {
+    $("#education").append(HTMLonlineStart);
     var keyOnline = education.onlineCourses[x];
     var formatOnlineTitle = HTMLonlineTitle.replace("%data%", keyOnline.title);
     var formatOnlineSchool = HTMLonlineSchool.replace("%data%", keyOnline.school);
     var formatTitleSchool = formatOnlineTitle + formatOnlineSchool;
-    $(".online-entry").append(formatTitleSchool);
-    addItems(".online-entry", HTMLonlineDates, keyOnline.dates);
-    addItems(".online-entry", (HTMLonlineURL.replace("#", keyOnline.url)), keyOnline.url);
+    $(".online-entry:last").append(formatTitleSchool);
+    addItems(".online-entry:last", HTMLonlineDates, keyOnline.dates);
+    addItems(".online-entry:last", (HTMLonlineURL.replace("#", keyOnline.url)), keyOnline.url);
   }
 
   //My Certifications
   $("#education").append(HTMLcert);
-  $("#education").append(HTMLcertStart);
   for (var y = 0; y < education.certifications.length; y++) {
+    $("#education").append(HTMLcertStart);
     var keyCert = education.certifications[y];
     var formatCertTitle = HTMLcertTitle.replace("%data%", keyCert.title);
     var formatCertSource = HTMLcertSource.replace("%data%", keyCert.source);
     var formatTitleSource = formatCertTitle + formatCertSource;
-    $(".certificate-entry").append(formatTitleSource);
-    addItems(".certificate-entry", HTMLcertDates, keyCert.dates);
-    addItems(".certificate-entry", (HTMLcertURL.replace("#", keyCert.url)), keyCert.url);
+    $(".certificate-entry:last").append(formatTitleSource);
+    addItems(".certificate-entry:last", HTMLcertDates, keyCert.dates);
+    addItems(".certificate-entry:last", (HTMLcertURL.replace("#", keyCert.url)), keyCert.url);
   }
 };
 
